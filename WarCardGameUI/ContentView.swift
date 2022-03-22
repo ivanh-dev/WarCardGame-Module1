27af00718ef97f13fb6a6d8718cb9a4ea732cd4f
Ivan Hirsinger <ivan.hirsinger@gmail.com>
2022-03-22 10:30:44 +0100

    Initial Commit

---
+++ WarCardGameUI/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  WarCardGameUI
+//
+//  Created by Ivan Hirsinger on 14.03.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    
+    var body: some View {
+        
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    
+                    // Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if playerRand < cpuRand {
+                        cpuScore += 1
+                    }
+                        
+                } label: {
+                    Image("dealbutton")
+                }
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack(spacing: 20.0) {
+                        Text("Player")
+                            .fontWeight(.bold)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack(spacing: 20.0) {
+                        Text("CPU")
+                            .fontWeight(.bold)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+
+                Spacer()
+
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ WarCardGameUI/WarCardGameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameUIApp.swift
+//  WarCardGameUI
+//
+//  Created by Ivan Hirsinger on 14.03.2022..
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
